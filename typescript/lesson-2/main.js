7eca2d3a7a785d89e8c1ab5d7316803640f9fd60
Rostislav Didyk <Rostislav.Didyk@viaamadeus.com>
2016-12-12 16:33:40 +0200

    add lesson-2 flickr task

--- typescript/lesson-2/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const App_1 = require('./App');
-(new App_1.App(document.getElementById('app'), '.js-app-template', '.js-result'));
-/// <reference path="lodash/index.d.ts" />
-/// <reference path="whatwg-fetch/index.d.ts" /> 
